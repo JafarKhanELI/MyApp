86db7b8d8f8aed7b2c32dc0667bd6a17f6b37ce3
jafarkhaneli <jafarkhan.sahib@healthicity.com>
2016-08-26 17:42:20 +0530

    first commit

---
+++ MyApp/Controllers/LinksController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MyApp.Models;
+using System.Web.Http.Description;
+using System.Web.Http.Cors;
+
+namespace MyApp.Controllers
+{
+    
+    [EnableCorsAttribute("*","*","*")]
+    public class LinksController : ApiController
+    {
+        // GET api/links
+        [ResponseType(typeof(LinkModel))]
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                var linkRepo = new LinksRepository();
+                return Ok(linkRepo.Retrive());
+            }
+            catch(Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        // GET api/links/5
+        [ResponseType(typeof(LinkModel))]
+        public IHttpActionResult Get(int id)
+        {
+            
+            try
+            {
+                var links = new LinksRepository().Retrive();
+                var link = links.FirstOrDefault(p => p.Id == id);
+                if (link == null)
+                    return NotFound();
+
+                return Ok(link);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+
+            }
+        }
+
+        // POST api/links
+        [ResponseType(typeof(LinkModel))]
+        public IHttpActionResult Post([FromBody]LinkModel link)
+        {
+            
+            try
+            {
+                if (link == null)
+                    return BadRequest("Product Cannont be null");
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                var savedLink =new LinksRepository().Save(link);
+                if (savedLink == null)
+                    return Conflict();
+                return Created<LinkModel>(Request.RequestUri + savedLink.Id.ToString(),savedLink);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+
+            }
+        }
+
+        // PUT api/links/5
+        public IHttpActionResult Put(int id, [FromBody]LinkModel link)
+        {
+            new LinksRepository().Save(id, link);
+
+            try
+            {
+                if (link == null)
+                    return BadRequest("Product Cannont be null");
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                var savedLink = new LinksRepository().Save(id,link);
+                if (savedLink == null)
+                    return NotFound();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+
+            }
+
+
+        }
+
+        // DELETE api/links/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MyApp/Models/LinksRepository.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+
+namespace MyApp.Models
+{
+    public class LinksRepository
+    {
+        string FilePath = HostingEnvironment.MapPath("~/App_Data/Links.json");
+
+        public List<LinkModel> Retrive()
+        {
+            var json = System.IO.File.ReadAllText(FilePath);
+
+            var links = JsonConvert.DeserializeObject<List<LinkModel>>(json);
+
+            return links;
+        }
+
+        public LinkModel Save(LinkModel link)
+        {
+            var links = Retrive();
+            var maxID = links.Max(p => p.Id);
+            link.Id = maxID + 1;
+            links.Add(link);
+            WriteData(links);
+            return link;
+        }
+
+        public LinkModel Save(int id,LinkModel link)
+        {
+            var links = Retrive();
+            var index = links.FindIndex(l => l.Id == id);
+            if (index >= 0)
+            {
+                links[index] = link;
+                WriteData(links);
+
+            }
+            else
+                return null;
+            return link;
+        }
+
+        public bool WriteData(List<LinkModel> links)
+        {
+            var json = JsonConvert.SerializeObject(links, Formatting.Indented);
+            
+            System.IO.File.WriteAllText(FilePath, json);
+
+            return true;
+        }
+    }
+}
